cb34658c20079a209b554ff02b71f36fa0a432fc
Uğur Yeter <ytr.ugur@gmail.com>
2016-09-20 11:18:15 +0300

    Update TestRecursion.java

--- Mavenized_Algorithms/src/test/java/com/unit/test/sort/TestRecursion.java
+++ Mavenized_Algorithms/src/test/java/com/unit/test/sort/TestRecursion.java
@@ -2,6 +2,7 @@ package com.unit.test.sort;
 
 import org.junit.Test;
 
+import com.algorithms.recursion.Euclidean;
 import com.algorithms.recursion.Factorial;
 
 import static org.junit.Assert.*;
@@ -30,6 +31,27 @@ public class TestRecursion {
 		assertEquals(120, cal);
 	}
 	
-
-
+	@Test 
+	public void testEuclideanZeroValue(){
+		int calculated = Euclidean.calculateGCD(100, 0);
+		assertEquals(0, calculated);
+	}
+	
+	@Test 
+	public void testEuclideanOneValue(){
+		int calculated = Euclidean.calculateGCD(1, 100);
+		assertEquals(1, calculated);
+	}
+	
+	@Test 
+	public void testEuclideanForSameValues(){
+		int calculated = Euclidean.calculateGCD(123, 123);
+		assertEquals(123, calculated);
+	}
+	
+	@Test 
+	public void testEuclideanForDiffValues(){
+		int calculated = Euclidean.calculateGCD(125, 35);
+		assertEquals(5, calculated);
+	}
 }
