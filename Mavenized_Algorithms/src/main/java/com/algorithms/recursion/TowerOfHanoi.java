237eb480d20a350e889dab6f8e617cc362c776d5
Uğur Yeter <ytr.ugur@gmail.com>
2016-09-22 11:01:38 +0300

    class is edited.

--- Mavenized_Algorithms/src/main/java/com/algorithms/recursion/TowerOfHanoi.java
+++ Mavenized_Algorithms/src/main/java/com/algorithms/recursion/TowerOfHanoi.java
@@ -1,21 +1,15 @@
 package com.algorithms.recursion;
 
 public class TowerOfHanoi {
-	
-	public void moveThemAll(int n, String from, String dest, String temp){
-		
-		if(n == 1 ) { // breakpoint of recursion
-			System.out.println(n+" moved From "+from +" To "+ dest );
-			return;
-		}		
-		moveThemAll(n-1, from,temp , dest); // The biggest piece make smaller ones move.
-		System.out.println(n+" moved From "+from +" To "+ dest );
-		moveThemAll(n-1, temp,dest , from); // The biggest piece then recall smaller ones.
-	}
-	
-	public static void main(String[] args) {
-		TowerOfHanoi  tao = new TowerOfHanoi();
-		tao.moveThemAll(3, "A", "C", "B");
-	}
 
+	public void moveThemAll(int n, String from, String dest, String temp) {
+
+		if (n == 1) { // breakpoint of recursion
+			System.out.println(n + " moved From " + from + " To " + dest);
+			return;
+		}
+		moveThemAll(n - 1, from, temp, dest); // The biggest piece make smaller ones move.
+		System.out.println(n + " moved From " + from + " To " + dest);
+		moveThemAll(n - 1, temp, dest, from); // The biggest piece then recall  smaller ones.
+	}
 }
