c083399bebac35d407a5473f249c11e92d2e98fe
Uğur Yeter <ytr.ugur@gmail.com>
2016-10-03 15:14:50 +0300

    Shell Sort Test functions.

--- Mavenized_Algorithms/src/test/java/com/unit/test/sort/QuickSortTest.java
+++ Mavenized_Algorithms/src/test/java/com/unit/test/sort/QuickSortTest.java
@@ -21,7 +21,6 @@ public class QuickSortTest {
 		int[] arrayOfInt = {3,8,2,5};
 		int[] expected = {2,3,5,8};
 		QuickSort.sortQuickly(arrayOfInt, 0, 3);
-		
 		assertArrayEquals(expected, arrayOfInt);
 	}
 	
@@ -31,7 +30,6 @@ public class QuickSortTest {
 		int[] arrayOfInt = {10,9,8,7,6,5,4,3,2,1};
 		int[] expected = {1,2,3,4,5,6,7,8,9,10};
 		QuickSort.sortQuickly(arrayOfInt, 0, 9);
-		
 		assertArrayEquals(expected, arrayOfInt);
 	}
 
