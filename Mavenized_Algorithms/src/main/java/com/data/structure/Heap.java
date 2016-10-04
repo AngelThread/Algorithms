af7239a571f064f8bdc92fae03037e4e938810ae
Uğur Yeter <ytr.ugur@gmail.com>
2016-10-04 17:10:04 +0300

    Heap structure class implementation.

---
+++ Mavenized_Algorithms/src/main/java/com/data/structure/Heap.java
@@ -0,0 +1,87 @@
+package com.data.structure;
+
+import java.lang.reflect.Array;
+
+public class Heap<E extends Comparable<E>> {
+	
+	private E [] arrayLocal = null;
+	private static final int Index_Of_Root=0;
+	private int size = 0;
+	private int elementNumb = 0;
+	
+	@SuppressWarnings("unchecked")
+	public Heap(Class<E> c,int size) {
+		arrayLocal = (E[]) Array.newInstance( c, size);
+		this.size = size;
+	}
+	
+	public void add(E e){
+
+		if(arrayLocal[Index_Of_Root] == null){
+			arrayLocal[Index_Of_Root] = e;
+			elementNumb++;
+			return;
+		}
+		if(isFull()) return;
+		arrayLocal[elementNumb] = e;
+		reArrenga(elementNumb);
+		elementNumb++;
+
+	}
+	
+	private boolean isFull(){
+		return (size- elementNumb) == 0;
+	}
+	
+	private void reArrenga(int indexIsAdded){
+		if(indexIsAdded <=0) return;
+		if(indexIsAdded == 0){
+			reArrenga(elementNumb);;
+		}
+		boolean isLeft = (indexIsAdded%2 == 1?true:false); // In the Heap Structure that's implemented by array object, Node's left child's index is 2i+1 and right one's 2i+2.
+		int parentIndex = -1;
+		if(isLeft){
+			parentIndex = (indexIsAdded-1)/2;
+		}else{
+			parentIndex = (indexIsAdded-2)/2;
+		}
+		if(parentIndex < 0) return;
+		if(arrayLocal[parentIndex]==null|| arrayLocal[parentIndex].compareTo(arrayLocal[indexIsAdded]) == -1){
+			E temp = arrayLocal[parentIndex];
+			arrayLocal[parentIndex] = arrayLocal[indexIsAdded];
+			arrayLocal[indexIsAdded] = temp;
+			reArrenga(parentIndex);
+		}
+	}
+
+	public E[] getArrayLocal() {
+		return arrayLocal;
+	}
+	public int getSize() {
+		return size;
+	}
+
+	public void delete(E elementWhatLookingFor){
+		int index = findElement(elementWhatLookingFor);
+		if(index == -1 ) return ;
+		arrayLocal[index]  = null;
+		if(index*2+2 < arrayLocal.length && arrayLocal[index*2+2] !=null ){
+			reArrenga(index*2+2 );
+		}else if(index*2+1 < arrayLocal.length && arrayLocal[index*2+1] !=null){
+			reArrenga(index*2+1);
+
+		}
+		
+		
+	}
+	
+	private int findElement(E elementWhatLookingFor){
+		for (int i = 0; i < arrayLocal.length; i++) {
+			if(elementWhatLookingFor.equals(arrayLocal[i]))
+				return i;
+		}	
+		
+		return -1;
+	}
+	
+}
